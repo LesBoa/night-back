ec4529a58943e7f165e8e308df8fe4f77e6ff68e
Nicolas Beaussart <nic.beaussart@gmail.com>
2018-12-07 02:49:00 +0100

    :white_check_mark: fixing tests

--- src/modules/health-info/health-info.dto.ts
+++ src/modules/health-info/health-info.dto.ts
@@ -8,7 +8,7 @@ import {
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiModelProperty } from '@nestjs/swagger';
-import { HealthType } from 'modules/health-type/health-type.entity';
+import { HealthType } from '../health-type/health-type.entity';
 
 export class HealthInfoDto {
   @IsNumber()
